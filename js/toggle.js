e073d299772670508b7e8e3f76c8cd13ffaf0f58
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2023-12-22 12:05:03 -0700

    major leap in popout navbar

--- js/toggle.js
+++ js/toggle.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
-	function toggleSidebar() {
-		$(".button").toggleClass("active");
-		$(".nav-Overflow").toggleClass("move-to-left");
-		$(".popout-Item").toggleClass("active");
-	}
+    function toggleSidebar() {
+        $("#hamburger-btn").toggleClass("active");
+        $(".popout-Nav").toggleClass("active");
+    }
 
-	$(".button").on("click tap", function () {
-		toggleSidebar();
-	});
+    $("#hamburger-btn").on("click tap", function () {
+        toggleSidebar();
+    });
 
-	$(document).keyup(function (e) {
-		if (e.keyCode === 27) {
-			toggleSidebar();
-		}
-	});
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27) {
+            toggleSidebar();
+        }
+    });
 });
