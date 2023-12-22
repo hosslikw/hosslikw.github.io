38bf7b8f91d19d55cbbbb7bb6a56c13dd3344e23
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2023-12-21 18:58:30 -0700

    page structure fixes

--- js/toggle.js
+++ js/toggle.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-  function toggleSidebar() {
-    $(".button").toggleClass("active");
-    $("main").toggleClass("move-to-left");
-    $(".sidebar-item").toggleClass("active");
-  }
+	function toggleSidebar() {
+		$(".button").toggleClass("active");
+		$(".nav-Overflow").toggleClass("move-to-left");
+		$(".popout-Item").toggleClass("active");
+	}
 
-  $(".button").on("click tap", function () {
-    toggleSidebar();
-  });
+	$(".button").on("click tap", function () {
+		toggleSidebar();
+	});
 
-  $(document).keyup(function (e) {
-    if (e.keyCode === 27) {
-      toggleSidebar();
-    }
-  });
+	$(document).keyup(function (e) {
+		if (e.keyCode === 27) {
+			toggleSidebar();
+		}
+	});
 });
