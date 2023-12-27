ed30b95587cb4b55c34a4585f3f85c56dbc60fd2
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2023-12-27 08:43:22 +0000

    working on Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,42 @@
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
-import pugPlugin from 'vite-plugin-pug';
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
 
-export default defineConfig({
-  plugins: [vue(), pugPlugin({ /* Pug plugin options */ })],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // SCSS global configurations
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './')
-    }
-  },
-  root: __dirname, // Set the project root to the current directory
-  publicDir: path.join(__dirname, 'public'), // Adjust if you have a public directory
-  build: {
-    outDir: path.join(__dirname, 'dist'), // Output directory for builds
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'), // Entry point for the build
-        // Add other entries if necessary
-      }
-    }
-  }
+// Define the configuration object
+export default defineConfig(async ({ command, mode }) => {
+  // Perform any asynchronous operations here
+  // const data = await asyncFunction()
+
+  return {
+    plugins: [
+      vue(),
+      pugPlugin({
+        /* Pug plugin options */
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // SCSS global configurations
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./"),
+      },
+    },
+    root: __dirname,
+    build: {
+      outDir: path.join(__dirname, "dist"),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+          // Add other entries if necessary
+        },
+      },
+    },
+  };
 });
