2705d2b9890ba16bbea4ab0de7cf742b6b71b928
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-05 03:33:20 -0700

    reorganize for postcss

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,34 @@
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
-import pugPlugin from 'vite-plugin-pug';
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// https://vitejs.dev/config/
-// Define the configuration object
-export default defineConfig(async ({ command, mode }) => {
-  // Perform any asynchronous operations here
-  // const data = await asyncFunction()
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-  return {
-    plugins: [
-      vue(),
-      vueJsx({}),
-      pugPlugin({
-        /* Pug plugin options */
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'), // Alias '@' points to the 'src' directory
-        '@root': path.resolve(__dirname, './'), // Alias '@root' points to the project root
-      },
-    },
-
-
-    root: __dirname,
-    build: {
-      outDir: path.join(__dirname, 'dist'),
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html'),
-          // Add other entries if necessary
-        },
-      },
-    },
-  };
-});
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx({}),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@root': path.resolve(__dirname, './')
+    }
+  },
+  root: __dirname,
+  build: {
+    outDir: path.join(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+        // Add other entries if necessary
+      }
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: ['e2e/*'], // you can adjust this exclude pattern to fit your needs
+    root: fileURLToPath(new URL('./', import.meta.url))
+  }
+})
