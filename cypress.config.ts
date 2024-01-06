764f6dd0d78a42d6606af82a9e3a6057b4682b62
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-06 07:43:37 -0700

    + some

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'cypress'
+/** @format */
+
+import { defineConfig } from "cypress"
 
 export default defineConfig({
-  e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
-  }
+	e2e: {
+		specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+		baseUrl: "http://localhost:4173"
+	}
 })
