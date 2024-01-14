4a07b2c07a1d8a519c6a699c5bbc9a0057e4523a
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-14 15:16:12 -0700

    fixing vite config

--- purgecss.config.mjs
+++ purgecss.config.mjs
@@ -1,33 +1,35 @@
-import { PurgeCSS } from 'purgecss';
-import { writeFile } from 'fs/promises';
-import path from 'path';
+/** @format */
+
+import { PurgeCSS } from "purgecss"
+import { writeFile } from "fs/promises"
+import path from "path"
 
 async function runPurgeCSS() {
-  try {
-    const content = ['/Users/Kyle/Sites/hosslikw.github.io/webkit-code-reference.html']; // Your HTML files
-    const css = ['/Users/Kyle/Sites/hosslikw.github.io/webkit-code-reference.css']; // Your CSS file
+	try {
+		const content = ["/Users/Kyle/Sites/hosslikw.github.io/webkit-code-reference.html"] // Your HTML files
+		const css = ["/Users/Kyle/Sites/hosslikw.github.io/webkit-code-reference.css"] // Your CSS file
 
-    // Running PurgeCSS
-    const result = await new PurgeCSS().purge({
-      content,
-      css,
-      // Don't specify the 'output' option here
-    });
+		// Running PurgeCSS
+		const result = await new PurgeCSS().purge({
+			content,
+			css
+			// Don't specify the 'output' option here
+		})
 
-    // Assuming there's only one CSS file in the result
-    if (result.length > 0 && result[0].css) {
-      const cssFilePath = css[0];
-      const cssDir = path.dirname(cssFilePath);
-      const outputFilePath = path.join(cssDir, 'purged.css'); // New filename
+		// Assuming there's only one CSS file in the result
+		if (result.length > 0 && result[0].css) {
+			const cssFilePath = css[0]
+			const cssDir = path.dirname(cssFilePath)
+			const outputFilePath = path.join(cssDir, "purged.css") // New filename
 
-      await writeFile(outputFilePath, result[0].css, 'utf-8');
-      console.log(`Purged CSS written to ${outputFilePath}`);
-    } else {
-      console.log('No CSS was purged.');
-    }
-  } catch (error) {
-    console.error('Error running PurgeCSS:', error);
-  }
+			await writeFile(outputFilePath, result[0].css, "utf-8")
+			console.log(`Purged CSS written to ${outputFilePath}`)
+		} else {
+			console.log("No CSS was purged.")
+		}
+	} catch (error) {
+		console.error("Error running PurgeCSS:", error)
+	}
 }
 
-runPurgeCSS();
+runPurgeCSS()
