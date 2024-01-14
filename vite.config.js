8fbb0bc3587fb32ab9b30cf6f23e21d4cef74658
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-14 15:16:08 -0700

    Pretend

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-/** @format */
-
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import path from "path"
 import { fileURLToPath } from "url"
+import purgecss from "vite-plugin-purgecss";
 
 // Get the current directory
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-	plugins: [vue(), vueJsx({})],
+	plugins: [
+	 [vue(), vueJsx({})],
+	 purgecss({
+      content: ['./**/*.html', './**/*.vue', './**/*.jsx', './**/*.tsx'],
+      // Other PurgeCSS options if needed
+    })
+  ],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
