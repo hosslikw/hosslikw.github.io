33bec9e53d6e7392d1d4970a085bf1da77ca567f
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-19 06:37:57 -0700

    cleaning before the dev push

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,37 +1,26 @@
-// ESM imports
-import postcss from "postcss";
-import autoprefixer from "autoprefixer";
-import cssnano from "cssnano";
-import postcssColorMod from "postcss-color-mod-function";
-import postcssImport from "postcss-import";
-import postcssModules from "postcss-modules";
-import postcssPresetEnv from "postcss-preset-env";
-import tailwindcss from "tailwind.config.js";
+import autoprefixer from "autoprefixer"
+import cssnano from "cssnano"
+import postcssImport from "postcss-import"
+import tailwindcss from "tailwindcss" // Ensure this package is installed
+import tailwindcssNesting from "tailwindcss/nesting/index.js" // Correct import path
 
-export default ({ env }) => {
-	const plugins =
-		env === "production"
-			? [
-				postcssImport({ path: "src/" }),
-				postcssFontFamilySystemUI({ fontFamily: { base: "system-ui, sans-serif" } }),
-				postcssModules({ generateScopedName: "[name]__[local]___[hash:base64:5]" }),
-				autoprefixer({
-					grid: "autoplace",
-					cascade: true,
-					remove: false,
-					flexbox: "true",
-					add: true,
-					supports: true
-				}),
-				cssnano({ preset: "advanced" })
+export default {
+	plugins: [
+		postcssImport,
+		tailwindcssNesting, // If you want to use "postcss-nesting", pass it as an argument
+		tailwindcss, // Ensure this package is installed
+		autoprefixer,
+		cssnano({
+			preset: [
+				"cssnano-preset-advanced",
+				{
+					cssDeclarationSorter: false,
+					calc: false,
+					mergeIndents: false,
+					reduceIndents: false,
+					zindex: false
+				}
 			]
-			: [
-				postcssImport({ path: "src/" }),
-				postcssColorMod({ importFrom: ["src/base.css"] }),
-				postcssPresetEnv({ stage: 2 }),
-				tailwindcss: { config: __dirname + '/tailwind.config.js' }
-
-			];
-
-	return { plugins };
-};
+		})
+	]
+}
