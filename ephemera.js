bf7cc950df0113212a47eee5310d32b8f927683a
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-21 22:00:03 -0700

    file nesting heaven

---
+++ ephemera.js
@@ -0,0 +1,101 @@
+/**
+ * Animates the movement and resizing of elements on a webpage based on user interactions and window resizing.
+ * @format
+ */
+
+global.$ = require("jquery")
+
+function parseNum(str) {
+	return parseFloat(str.replace(/[^\d.-]/g, ""))
+}
+
+function rand(min, max) {
+	return Math.random() * (max - min) + min
+}
+
+function randSign() {
+	let r = Math.round(Math.random())
+	if (r == 0) r = -1
+	return r
+}
+
+function startEph() {
+	if ($(window).width() >= 800) {
+		let n = $(".eph").length
+		let windowW = $("#Efemera").width() / 4
+		let ratio = windowW / $(".eph").eq(0).width()
+		let width, height
+		let top = parseNum($("#Efemera").css("top"))
+		let left = parseNum($("#Efemera").css("left"))
+
+		if ($(window).width() < 800) {
+			width = 200
+			height = 150
+		} else {
+			width = $(".eph").eq(0).width() * ratio
+			height = $(".eph").eq(0).height() * ratio
+		}
+
+		let rMin = 50 * ratio,
+			rMax = 100 * ratio,
+			nCols = 3
+
+		for (let i = 0; i < n; ++i) {
+			let tile = $(".eph").eq(i)
+			let topNow = top + rand(rMin, rMax) * randSign()
+			if (topNow + height > $(window).height()) {
+				topNow = $(window).height() - height - 5
+			} else if (topNow < 0) {
+				topNow = 5
+			}
+
+			tile.animate({ width: width, height: height, top: topNow }, { duration: 250, queue: false })
+
+			if (i % nCols == 1) {
+				tile.animate({ width: width, height: height, left: rand(rMin, rMax) }, { duration: 250, queue: false })
+				top = top + (height * ratio) / 3
+			} else if (i % nCols != 0) {
+				let leftNow = left * (i % nCols) + rand(rMin, rMax) * randSign()
+				if (leftNow + width > $(window).width()) {
+					leftNow = $(window).width() - width - 5
+				}
+				tile.animate({ width: width, height: height, left: leftNow }, { duration: 250, queue: false })
+				top = top + (height * ratio) / 3
+			}
+		}
+
+		$("#Efemera").css("height", top)
+		$("body").css("height", top)
+		$("#Efemera").css("width", left)
+	} else {
+		$(".eph").removeAttr("style")
+	}
+	$(".eph").css("visibility", "visible")
+}
+
+function closeEph(dur) {
+	if ($(window).width() >= 800) {
+		dur = typeof dur !== "undefined" ? dur : 250
+		let n = $(".eph").length
+		let top = parseNum($("#Efemera").css("top"))
+		let left = parseNum($("#Efemera").css("left"))
+
+		for (let i = 0; i < n; ++i) {
+			let tile = $(".eph").eq(i)
+			tile.animate({ width: 200, height: 150, top: top, left: left }, { duration: dur, queue: false })
+			top += rand(10, 30) * randSign()
+			left += rand(10, 30) * randSign()
+		}
+	} else {
+		$(".eph").removeAttr("style")
+		$("#Efemera").height($(window).height())
+	}
+	$(".eph").css("visibility", "visible")
+}
+
+$(document).ready(function () {
+	closeEph(0)
+	$(window).resize(closeEph)
+	$(".eph").on("mouseenter", startEph)
+	$("body").on("mouseleave", closeEph)
+})
---
+++ index.js
@@ -0,0 +1,106 @@
+import "../css/main.css"
+
+/**
+ * The showImage function updates the source of an image element, displays a
+ * large image panel, and makes a "Click to Enlarge" button visible.
+ * @param imgName - The parameter "imgName" is a string that represents the name or path of the image
+ * file that you want to display.
+ */
+function showImage(imgName) {
+	document.getElementById("largeImg").src = imgName
+	showLargeImagePanel(imgName)
+	document.getElementById("ClickEnlargePhoto").style.visibility = "visible"
+	unselectAll()
+}
+
+function showLargeImagePanel(imgName) {
+	let imgH, imgW, w2, h2
+	for (let i = 0; i < $("#middle img.fig").length; ++i) {
+		let item = $("#middle img.fig:eq(" + i + ")")
+		if (item.attr("src") == imgName) {
+			imgH = $(item).height()
+			imgW = $(item).width()
+			break
+		}
+	}
+	let w = $(window).width()
+	let h = $(window).height()
+	if (imgH == undefined) {
+		imgH = h
+		imgW = w
+	}
+	if (imgW > imgH && (w < h || (w > h && w / h < imgW / imgH))) {
+		h2 = w * (imgH / imgW)
+		$("#ClickEnlargePhoto").css({ height: h2, width: w, top: (h - h2) / 2, left: 0 })
+		$("#largeImg").css({ height: h2 - 4, width: w - 4 })
+	} else {
+		w2 = h * (imgW / imgH)
+		$("#ClickEnlargePhoto").css({ width: w2, height: h, left: (w - w2) / 2, top: 0 })
+		$("#largeImg").css({ width: w2 - 4, height: h - 4 })
+	}
+}
+
+/**
+ * Unselects all text and then hides an element.
+ */
+function unselectAll() {
+	if (document.selection) document.selection.empty()
+	if (window.getSelection) window.getSelection().removeAllRanges()
+}
+
+function hideMe(obj) {
+	obj.style.visibility = "hidden"
+}
+
+/**
+ * Adjusts the height of certain elements on the page based on the window height
+ * and other factors.
+ */
+let computeDocHeight = function () {
+	if (document.getElementById("ClickEnlargePhoto").style.visibility == "visible") {
+		showLargeImagePanel($("#largeImg").attr("src"))
+	}
+	let h = $(window).height()
+	let hdrHeight = $("#Home_link").height()
+	$("#left2").css("height", h - hdrHeight - 50)
+	if ($(".video").length != 0) {
+		let width = $("#middle").width()
+		$(".video").css({ width: width, height: width * 0.5625 })
+	}
+	if ($(".video2").length != 0) {
+		let width = $("#middle").width() / 3
+		$(".video2").css({ width: width, height: width * 0.5625 })
+	}
+}
+$(".Links_ArtworkInitials ").on("mouseenter", function () {
+	let work = $(this)[0].classList[1]
+	$("." + work).css("text-decoration", "line-through")
+	let first = "." + work + ":eq(0)"
+	let second = "." + work + ":eq(1)"
+	if ($(second).length > 0) {
+		$(second).animate({ "opacity": 0.0, "margin-left": "-100px" }, 800, setInvisible)
+		$(second).animate({ "opacity": 1.0, "margin-left": "0px" }, 10, setVisible)
+	}
+	function setInvisible() {
+		$(this).css("visibility", "hidden")
+	}
+	function setVisible() {
+		$(this).css("visibility", "visible")
+	}
+})
+
+$(".Links_ArtworkInitials ").on("mouseleave", function () {
+	let work = $(this)[0].classList[1]
+	$("." + work).css("text-decoration", "none")
+})
+
+$(document).ready(computeDocHeight)
+$(window).resize(computeDocHeight)
+document.querySelector(".svg-wrapper").addEventListener("click", function () {
+	// place animation here
+})
+
+/**
+ * Takes a string as input and returns a parsed floating-point number by
+ * removing the last two characters from the string.
+ *@param string*/
