42184574e46af9435bf0f1df952435e1ba3046c0
Kyle H. <36752421+hosslikw@users.noreply.github.com>
2024-01-22 12:26:15 -0700

    sanity

---
+++ sanity-cms/schemas/index.js
@@ -0,0 +1,15 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import pet from "./pet"
+import testimonials from "./testimonials"
+
+
+
+export default createSchema({
+   name: "default",
+   types: schemaTypes.concat([
+      pet,
+      testimonials
+   ]),
+})
