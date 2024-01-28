4d3138d51fa577a28e075951428b1d44503db48d
Kyle H <36752421+hosslikw@users.noreply.github.com>
2024-01-28 12:51:04 -0700

    settigns fallout

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,51 @@
-/// <reference types="vitest" />
-import vuePlugin from "@vitejs/plugin-vue"
-import vueJsx from "@vitejs/plugin-vue-jsx"
-import path from "node:path"
-import { fileURLToPath, URL } from "node:url"
-import { defineConfig } from "vite"
+/// <reference types='vitest' />
+import vuePlugin from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
 
 // Get the directory name in ESM syntax
-const __dirname = fileURLToPath(new URL(".", import.meta.url))
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-export default defineConfig(({ mode }) => {
-	const base = "/"
+const config = defineConfig(({ mode }) => {
+	const base = '/'
 
 	return {
 		base,
 		plugins: [
 			vuePlugin(),
-			vueJsx()
+			vueJsx(),
+			Vue()
 		],
 		resolve: {
 			alias: {
-				"@": fileURLToPath(new URL("./", import.meta.url))
+				'@': fileURLToPath(new URL('./', import.meta.url))
 			}
 		},
 		build: {
-			minify: mode === "production",
-			outDir: path.join(__dirname, "dist"),
+			minify: mode === 'production',
+			outDir: path.join(__dirname, 'dist'),
 			emptyOutDir: true,
 			rollupOptions: {
 				input: {
-					main: path.resolve("src/15-sa.html")
+					main: path.resolve('src/15-sa.html')
 				}
 			},
 			ssr: {
-				noExternal: ["@vitejs/test-example-external-component"]
+				noExternal: ['@vitejs/test-example-external-component']
 			}
 		},
 		server: {
-			port: 3001,
-			open: "/src/15-sa.html"
+			open: 'src/15-sa.html'
 		},
 		root: __dirname,
 		test: {
-			environment: "jsdom",
-			exclude: ["e2e/*"]
+			environment: 'jsdom',
+			exclude: ['e2e/*']
 		}
 	}
 })
+
+export default config;
