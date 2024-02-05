3c7659b14cbd5c24cc4e3df0bd51c03d1f1afd4e
Kyle Hossli <hosslikw@gmail.com>
2024-02-05 03:29:16 -0700

    repairing npm script paths

--- src/togl.js
+++ src/togl.js
@@ -5,11 +5,11 @@ import ToglClassic from "./togl-classic.vue"
 
 export default defineComponent({
 	components: {
-		ToglBloc
-		, ToglBloc1
-		, ToglClassic
-	}
-	, mounted() {
+		ToglBloc,
+		ToglBloc1,
+		ToglClassic
+	},
+	mounted() {
 		if (import.meta.env.MODE === "development") {
 			// Assuming devtools is a valid object with a connect method
 		}
