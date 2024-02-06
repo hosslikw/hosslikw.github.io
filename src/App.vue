1502e03ed130d693896aa3b02aff0adc90dc570f
Kyle Hossli <hosslikw@gmail.com>
2024-02-06 16:52:16 -0700

    coming together, html and css for survey pg

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <h1>{{ message }}</h1>
+   <togl-bloc />
+   <togl-bloc1 />
+   <togl-classic />
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         message: 'This is some text'
+      };
+   }
+};
+</script>
+
+<style>
+#app>div {
+   border: dashed black 1px;
+   display: inline-block;
+   margin: 10px;
+   padding: 10px;
+   background-color: lightgreen;
+}
+</style>
