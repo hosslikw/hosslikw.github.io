6f5dec89d48ecf48c259fca0fdcdb4523575da26
Kyle Hossli <hosslikw@gmail.com>
2024-02-08 15:14:11 -0700

    moving

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 Vue.component("click-counter", {
-	template: '<button @click="count++">{{count}}</button>',
-	data() {
-		return {
-			count: 0
-		}
-	}
+template: '<button @click="count++">{{count}}</button>'
+, data() {
+return {
+count: 0
+}
+}
 })
 new Vue({
-	el: "#app"
+el: "#app"
 })
 //first method
