86a40dba9896273a6d3dd531a2849fdc52315ac7
Kyle Hossli <hosslikw@gmail.com>
2024-02-12 15:13:54 -0700

    system fonts

--- src/app2.js
+++ src/app2.js
@@ -10,3 +10,15 @@ new Vue({
 el: "#app"
 })
 //second method
+Vue.component("ClickCounter", {
+   template: '<button @click="count++">{{count}}</button>'
+   , data() {
+      return {
+         count: 0
+      }
+   }
+})
+new Vue({
+   el: "#app"
+})
+//first method
