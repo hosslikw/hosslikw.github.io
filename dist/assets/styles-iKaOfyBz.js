cf7a19dca3c3d7c3a0059d76cec7cbb6a26e7494
Kyle Hossli <hosslikw@gmail.com>
2024-02-27 22:51:01 -0500

    component organization

--- dist/assets/styles-iKaOfyBz.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";Vue.component("ClickCounter",{template:'<button @click="count++">{{count}}</button>',data(){return{count:0}}});new Vue({el:"#app"});Vue.component("ClickCounter",{template:'<button @click="count++">{{count}}</button>',data(){return{count:0}}});new Vue({el:"#app"});
