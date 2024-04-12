f20d69c28231ffc59d57ebb61de41360e4e0db10
Kyle Hossli <hosslikw@gmail.com>
2024-04-12 03:36:30 -0600

    committed

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,55 +1,55 @@
-const reactRecommended = require('eslint-plugin-react/configs/recommended');
-const globals = require('globals');
+// const reactRecommended = require('eslint-plugin-react/configs/recommended');
+// const globals = require('globals');
 
-module.exports = [
-  …
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    ...reactRecommended,
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    languageOptions: {
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser,
-      },
-    },
-  },
-  …
-];
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/base",
-    "plugin:vue/vue3-strongly-recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:markdown/recommended",
-    "plugin:cypress/recommended",
-    "plugin:json/recommended",
-    "plugin:jsonc/recommended",
-    "plugin:babel/recommended",
-    "plugin:prettier/recommended",
-    "plugin:html/recommended",
-    "plugin:tailwind/recommended",
-    "plugin:react/recommended"
-  ],
-  rules: js.configs.recommended.rules
+// module.exports = [
+//   …
+//   {
+//     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+//     ...reactRecommended,
+//   },
+//   {
+//     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+//     languageOptions: {
+//       globals: {
+//         ...globals.serviceworker,
+//         ...globals.browser,
+//       },
+//     },
+//   },
+//   …
+// ];
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:vue/base",
+//     "plugin:vue/vue3-strongly-recommended",
+//     "plugin:jsx-a11y/recommended",
+//     "plugin:markdown/recommended",
+//     "plugin:cypress/recommended",
+//     "plugin:json/recommended",
+//     "plugin:jsonc/recommended",
+//     "plugin:babel/recommended",
+//     "plugin:prettier/recommended",
+//     "plugin:html/recommended",
+//     "plugin:tailwind/recommended",
+//     "plugin:react/recommended"
+//   ],
+//   rules: js.configs.recommended.rules
 
-  files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-  parserOptions: {
-    sourceType: "script",
-  },
-},
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    parser: "eslint/parser",
-    sourceType: "module",
-  },
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-  },
-}
+//   files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+//   parserOptions: {
+//     sourceType: "script",
+//   },
+// },
+//   ],
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//     parser: "eslint/parser",
+//     sourceType: "module",
+//   },
+//   rules: {
+//     indent: ["error", "tab"],
+//     "linebreak-style": ["error", "unix"],
+//     quotes: ["error", "double"],
+//     semi: ["error", "never"],
+//   },
+// }
