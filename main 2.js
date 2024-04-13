dcfd59ec11635ebced471d57c0807b6fe192c299
Kyle Hossli <hosslikw@gmail.com>
2024-04-13 10:29:03 -0600

    still reeling after getting f@# by github actions

--- main 2.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue"
-import App from "../src/components/views/App.vue"
-import router from "./router"
-
-const app = createApp(App) // Use createApp here
-app.use(router)
-app.mount("#app")
