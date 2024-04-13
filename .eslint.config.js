571a68411e2bbc058b29a4e11e9cccd7090fcf0b
Kyle Hossli <hosslikw@gmail.com>
2024-04-12 22:40:53 -0600

    eslint explored
    New language describing the GitHub action that deploys the website.
    I forked a template for a GitHub action that might be able to track the project's version number.
    I changed the dependa-bot action, now it fully executes despite if the build process fails

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,116 +1,30 @@
-<<<<<<< HEAD
-// const reactRecommended = require('eslint-plugin-react/configs/recommended');
-// const globals = require('globals');
-
-// module.exports = [
-//   …
-//   {
-//     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-//     ...reactRecommended,
-//   },
-//   {
-//     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-//     languageOptions: {
-//       globals: {
-//         ...globals.serviceworker,
-//         ...globals.browser,
-//       },
-//     },
-//   },
-//   …
-// ];
-//   extends: [
-//     "eslint:recommended",
-//     "plugin:vue/base",
-//     "plugin:vue/vue3-strongly-recommended",
-//     "plugin:jsx-a11y/recommended",
-//     "plugin:markdown/recommended",
-//     "plugin:cypress/recommended",
-//     "plugin:json/recommended",
-//     "plugin:jsonc/recommended",
-//     "plugin:babel/recommended",
-//     "plugin:prettier/recommended",
-//     "plugin:html/recommended",
-//     "plugin:tailwind/recommended",
-//     "plugin:react/recommended"
-//   ],
-//   rules: js.configs.recommended.rules
-
-//   files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-//   parserOptions: {
-//     sourceType: "script",
-//   },
-// },
-//   ],
-//   parserOptions: {
-//     ecmaVersion: "latest",
-//     parser: "eslint/parser",
-//     sourceType: "module",
-//   },
-//   rules: {
-//     indent: ["error", "tab"],
-//     "linebreak-style": ["error", "unix"],
-//     quotes: ["error", "double"],
-//     semi: ["error", "never"],
-//   },
-// }
-=======
 import js from "@eslint/js";
+import eslintConfigESLint from "eslint-config-eslint";
+
 export default [
-    js.configs.recommended,
-    (
-        rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "warn"
-        }
-   }
-];
+    ...eslintConfigESLint,
 
-
-onst reactRecommended = require("eslint-plugin-react/configs/recommended")
-const globals = require("globals")
-
-module.exports = [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-    languageOptions: {
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser,
-      },
+    // apply recommended rules to JS files
+    {
+        files: ["**/*.js"],
+        rules: js.configs.recommended.rules
     },
-  },
-]
-module.exports = {
-  extends: [
-    "eslint-plugin-eslint-comments/recommended",
-    "eslint:recommended",
-    "plugin:vue/base",
-    "plugin:vue/vue3-strongly-recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:markdown/recommended",
-    "plugin:cypress/recommended",
-    "plugin:json/recommended",
-    "plugin:jsonc/recommended",
-    "plugin:babel/recommended",
-    "plugin:prettier/recommended",
-    "plugin:html/recommended",
-    "plugin:tailwind/recommended",
-    "plugin:react/recommended",
-  ],
-  rules: {
-    ...reactRecommended.rules,
-  },
-  parserOptions: {
-    ecmaVersion: "latest",
-    parser: "eslint/parser",
-    sourceType: "module",
-  },
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-  },
-}
->>>>>>> 1a43bc78 (sync)
+
+    // apply recommended rules to JS files with an override
+    // {
+    //     files: ["**/*.js"],
+    //     rules: {
+    //         ...js.configs.recommended.rules,
+    //         "no-unused-vars": "warn"
+    //     }
+    // },
+
+    // apply all rules to JS files
+    // {
+    //     files: ["**/*.js"],
+    //     rules: {
+    //         ...js.configs.all.rules,
+    //         "no-unused-vars": "warn"
+    //     }
+    // }
+];
