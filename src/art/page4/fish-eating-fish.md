7df683aa1c28dee6c72ad92f9c57ba8ca5866389
Kyle Hossli <hosslikw@gmail.com>
2024-04-22 16:09:10 -0600

    restructuring based on vite config

--- src/art/page4/fish-eating-fish.md
+++
@@ -1,1 +0,0 @@
-# Fish Eating Fish
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ const __dirname = fileURLToPath(new URL(".", import.meta.url))
 const includePath = normalizePath("src\\components")
 
 export default defineConfig(({ mode }) => {
+  define : {
+    __BASE_URL__: JSON.stringify('https://kylehossli.com/')
+    __BUILD_TIME__: JSON.stringify(new Date().toISOString())
+  }
   return {
     plugins: [
       deadFile({
@@ -20,7 +24,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@": __dirname, // Directly use __dirname if you meant to point to the project root
-        "@images": path.resolve(__dirname, "src/images"),
+        "@images": path.resolve(__dirname, "public/images"),
         "@styles": path.resolve(__dirname, "src/styles"),
       },
     },
