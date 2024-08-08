255ebcadcbfda690bb3ee48493c3c8af8cd8c912
Kyle Hossli <hosslikw@gmail.com>
2024-08-08 00:01:23 -0600

    change vite.config to exclude inputs to try and cut down on image duplication

--- vite.config.js
+++ vite.config.js
@@ -41,31 +41,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      rollupOptions: {
-        input: {
-          '01-uniform': path.resolve(__dirname, "src/html/art/01-uniform.html"),
-          '02-the-brass': path.resolve(__dirname, "src/html/art/02-the-brass.html"),
-          '03-the-bottom-is-aluminum-too': path.resolve(__dirname, "src/html/art/03-the-bottom-is-aluminum-too.html"),
-          '04-hump': path.resolve(__dirname, "src/html/art/04-hump.html"),
-          '05-riptide-rush': path.resolve(__dirname, "src/html/art/05-riptide-rush.html"),
-          '06-happy-meal': path.resolve(__dirname, "src/html/art/06-happy-meal.html"),
-          '07-faking-it-with-family': path.resolve(__dirname, "src/html/art/07-faking-it-with-family.html"),
-          '08-sign-fold': path.resolve(__dirname, "src/html/art/08-sign-fold.html"),
-          '09-late-night': path.resolve(__dirname, "src/html/art/09-late-night.html"),
-          '10-she': path.resolve(__dirname, "src/html/art/10-she.html"),
-          '11-lush': path.resolve(__dirname, "src/html/art/11-lush.html"),
-          '12-concrete': path.resolve(__dirname, "src/html/art/12-concrete.html"),
-          '13-do-the-right-thing': path.resolve(__dirname, "src/html/art/13-do-the-right-thing.html"),
-          '14-kwh': path.resolve(__dirname, "src/html/art/14-kwh.html"),
-          '15-rope': path.resolve(__dirname, "src/html/art/15-rope.html"),
-          '16-staged-authority': path.resolve(__dirname, "src/html/art/16-staged-authority.html"),
-          '17-robert-smith': path.resolve(__dirname, "src/html/art/17-robert-smith.html"),
-          '18-helium': path.resolve(__dirname, "src/html/art/18-helium.html"),
-          '19-institute-for-recurrent-progress': path.resolve(__dirname, "src/html/art/19-institute-for-recurrent-progress.html"),
-          '20-cards-against-humanity': path.resolve(__dirname, "src/html/art/20-cards-against-humanity.html"),
-          '21-fish-eating-fish': path.resolve(__dirname, "src/html/art/21-fish-eating-fish.html"),
-        },
-      },
+
       sourcemap: mode !== "production", // Enable sourcemaps in non-production modes
       manifest: true,
     },
