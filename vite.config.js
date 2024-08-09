3776821a84bf9dd3055ff148f52a8da0bc580f0a
Kyle Hossli <hosslikw@gmail.com>
2024-08-09 02:17:07 -0600

    individualizing the vite input entries

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath } from 'node:url';
 import path from 'path';
 import { defineConfig } from 'vite';
 import dns from 'node:dns'
-import glob from 'glob';
 
 // Convert file URL to directory path
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
@@ -25,10 +24,30 @@ export default defineConfig({
     sourcemap: true,
     manifest: true,
     rollupOptions: {
-      input: glob.sync('./src/html/art/*.html').reduce((acc, file) => {
-        acc[path.basename(file, '.html')] = file;
-        return acc;
-      }, {}),
+      input: {
+          '01-uniform': `${__dirname}/src/html/art/01-uniform.html`,
+          '02-the-brass': `${__dirname}/src/html/art/02-the-brass.html`,
+          '03-the-bottom-is-aluminum-too': `${__dirname}/src/html/art/03-the-bottom-is-aluminum-too.html`,
+          '04-hump': `${__dirname}/src/html/art/04-hump.html`,
+          '05-riptide-rush': `${__dirname}/src/html/art/05-riptide-rush.html`,
+          '06-happy-meal': `${__dirname}/src/html/art/06-happy-meal.html`,
+          '07-faking-it-with-family': `${__dirname}/src/html/art/07-faking-it-with-family.html`,
+          '08-sign-fold': `${__dirname}/src/html/art/08-sign-fold.html`,
+          '09-late-night': `${__dirname}/src/html/art/09-late-night.html`,
+          '10-she': `${__dirname}/src/html/art/10-she.html`,
+          '11-lush': `${__dirname}/src/html/art/11-lush.html`,
+          '12-concrete': `${__dirname}/src/html/art/12-concrete.html`,
+          '13-do-the-right-thing': `${__dirname}/src/html/art/13-do-the-right-thing.html`,
+          '14-kwh': `${__dirname}/src/html/art/14-kwh.html`,
+          '15-rope': `${__dirname}/src/html/art/15-rope.html`,
+          '16-staged-authority': `${__dirname}/src/html/art/16-staged-authority.html`,
+          '17-robert-smith': `${__dirname}/src/html/art/17-robert-smith.html`,
+          '18-helium': `${__dirname}/src/html/art/18-helium.html`,
+          '19-institute-for-recurrent-progress': `${__dirname}/src/html/art/19-institute-for-recurrent-progress.html`,
+          '20-cards-against-humanity': `${__dirname}/src/html/art/20-cards-against-humanity.html`,
+          '21-fish-eating-fish': `${__dirname}/src/html/art/21-fish-eating-fish.html`,
+          'index': `${__dirname}/src/html/index.html`,
+      },
     },
   },
   resolve: {
