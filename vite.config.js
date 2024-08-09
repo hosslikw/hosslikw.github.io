4025d7d15d37c2c6efc2f7114c480e2d2a81f1ca
Kyle Hossli <hosslikw@gmail.com>
2024-08-08 23:10:19 -0600

    vite.config updates to reposition mani.js and styles.css

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,28 @@
 import { fileURLToPath } from 'node:url';
 import path from 'path';
 import { defineConfig } from 'vite';
+import dns from 'node:dns'
 
 // Convert file URL to directory path
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
-export default defineConfig(({ mode }) => {
+dns.setDefaultResultOrder('verbatim')
   // Define global constants based on environment
-  const globalDefines = {
+const globalDefines = {
     __BASE_URL__: JSON.stringify('https://kylehossli.com/'),
     __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
   };
 
-  return {
-    build: {
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html'),
-        },
-      },
-      sourcemap: mode !== 'production', // Enable sourcemaps in non-production modes
-      manifest: true,
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    manifest: true,
+  },
+  resolve: {
+    alias: {
+      '@': __dirname, // Alias for project root
+      '@images': path.resolve(__dirname, 'public/images'),
+      '@styles': path.resolve(__dirname, 'src/css/'),
     },
-    resolve: {
-      alias: {
-        '@': __dirname, // Alias for project root
-        '@images': path.resolve(__dirname, 'public/images'),
-        '@styles': path.resolve(__dirname, 'src/css'),
-      },
-    },
-    define: globalDefines, // Add global defines here
-  };
+  },
+  define: globalDefines, // Add global defines here
 });
